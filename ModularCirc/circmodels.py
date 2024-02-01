55209244bcb4f8cf93214d75cb942b491233d0e4
Maximilian Balmus <balmus.maximilian@gmail.com>
2024-02-01 09:57:07 +0000

    circmodels: introducing some of the derived types

--- ModularCirc/circmodels.py
+++ ModularCirc/circmodels.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sympy as sp
-from .timeclass import TimeSeries
+# from .timeclass import TimeSeries
 
 
 def resistor_model_flow(p_in:float, p_out:float, r:float) -> float:
@@ -58,7 +58,7 @@ def softplus(val:float, alpha:float) -> float:
     else:
         return val
     
-def get_softplus_max(alpha:float) -> function:
+def get_softplus_max(alpha:float):
     """
     Method for generating softmax lambda function based on predefined alpha values
     
@@ -72,7 +72,7 @@ def get_softplus_max(alpha:float) -> function:
     """
     return lambda val : softplus(val=val, alpha=alpha)
 
-def non_ideal_diode_flow(p_in:float, p_out:float, r:float, max_func:function=relu_max) -> float:
+def non_ideal_diode_flow(p_in:float, p_out:float, r:float, max_func=relu_max) -> float:
     """
     Nonideal diode model with the option to choose the re
 
@@ -171,18 +171,221 @@ def chamber_exponential_law(v:float, E:float, k:float, v_ref:float, *args, **kwa
         float: chamber pressure
     """
     return E * np.exp(k * (v - v_ref) - 1)
-    
 
-class RLSeriesModel():
-    def __init__(self, time_series : TimeSeries) -> None:
-        self.time_series = time_series
+TEMPLATE_TIME_SETUP_DICT = {
+    'name'    :  'generic',
+    'ncycles' :  5,
+    'tcycle'  :  1.0,
+    'dt'      :  0.1
+ }
+
+class TimeClass():
+    def __init__(self, time_setup_dict) -> None:
+        self._time_setup_dict = time_setup_dict
+        self._initialize_time_array()
+        self.cti = 0 # current time step index
         
-        p_in, p_out, q = sp.symbols('p_in, p_out, q')
-        dqdt = sp.symbols('dqdt')
-        dt   = sp.symbols('dt')
+    @property
+    def ncycles(self):
+        if 'ncycles' in self._time_setup_dict.keys():
+            return self._time_setup_dict['ncycles']
+        else: 
+            return None
+    
+    @property 
+    def tcycle(self):
+        if 'tcycle' in self._time_setup_dict.keys():
+            return self._time_setup_dict['tcycle']
+        else:
+            return None
+    
+    @property
+    def dt(self):
+        if 'dt' in self._time_setup_dict.keys():
+            return self._time_setup_dict['dt']
+        else:
+            return None
+    
+    def _initialize_time_array(self):
+        # discretization of on heart beat, used as template
+        self._cycle_t = np.linspace(
+            start= 0.0,
+            stop = self.tcycle,
+            num  = int(self.tcycle / self.dt)+1,
+            dtype= np.float64
+            )
         
-        L, R = sp.symbols('L, R')
+        # discretization of the entire simulation duration
+        self._sym_t = np.array(
+            [t+cycle*self.tcycle for cycle in range(self.ncycles) for t in self._cycle_t[:-1]]
+        )
         
-        formula = (p_in - p_out) / dt - q - L / R * dqdt
+        # array of the current time within the heart cycle
+        self._sym_t_norm = np.array(
+            [t for _ in range(self.ncycles) for t in self._cycle_t[:-1]]
+        )
         
-        print(formula)
+        # the total number of time steps including initial time step
+        self.n_t = len(self._sym_t)
+        
+    def new_time_step(self):
+        self.cti += 1
+        
+
+class Chamber():
+    def __init__(self, 
+                 time_object:TimeClass, 
+                #  main_var:str
+                 ) -> None:
+        self._state_var = dict() 
+        self._to      = time_object
+        self.main_var = main_var
+        
+        self._state_var['P_i'] = np.zeros((time_object.n_t,))
+        self._state_var['P_o'] = np.zeros((time_object.n_t,))
+        self._state_var['Q_i'] = np.zeros((time_object.n_t,))
+        self._state_var['Q_o'] = np.zeros((time_object.n_t,))
+        
+    @property
+    def P_i(self):
+        return self._state_var['P_i']
+    
+    @property
+    def P_o(self):
+        return self._state_var['P_o']
+    
+    @property
+    def Q_i(self):
+        return self._state_var['Q_i']
+    
+    @property
+    def Q_o(self):
+        return self._state_var['Q_o']
+    
+    def comp_dvdt(self, ind):
+        return self.Q_i[ind] - self.Q_o[ind]
+    
+    def comp_v(self, q_i, q_o, v_prev):
+        return v_prev  + self._to.dt * self.comp_dvdt(q_i, q_o)      
+        
+        
+class Rc_component(Chamber):
+    def __init__(self, 
+                 time_object:TimeClass, 
+                #  main_var:str, 
+                 r:float, 
+                 c:float, 
+                 v_ref:float
+                 ) -> None:
+        # super().__init__(time_object, main_var)
+        super().__init__(time_object)
+        self._state_var['V']   = np.zeros((time_object.n_t,))
+        self.R = r
+        self.C = c
+        self.V_ref = v_ref
+        
+    @property
+    def V(self):
+        return self._state_var['V']
+        
+    def comp_dpdt(self, Q_in:float, Q_out:float):
+        return 1.0 /  self.C * (Q_in - Q_out)
+    
+    def comp_p(self, V):
+        return (V - self.V_ref) / self.C
+    
+    
+class Valve_leaky_diode(Chamber):
+    def __init__(self, 
+                 time_object: TimeClass,
+                 r:float, 
+                 max_func
+                 ) -> None:
+        super().__init__(time_object)
+        self.R = r
+        self._state_var['Q_o'] = self.Q_i
+        self.max_func = max_func
+        
+    def comp_q(self, p_i, p_o):
+        return self.max_func(p_i - p_o) / self.R
+    
+class HC_constant_elastance(Chamber):
+    def __init__(self, 
+                 time_object: TimeClass,
+                 E_pas: float, 
+                 E_act: float,
+                 activation_function_template = activation_function_1,
+                 *args, **kwargs
+                 ) -> None:
+        super().__init__(time_object)
+        self._af = lambda t : activation_function_template(t, *args, **kwargs)
+        self.E_pas = E_pas
+        self.E_act = E_act
+        self.eps = 1.0e-3
+        
+        self._state_var['V']   = np.zeros((time_object.n_t,))
+        
+    def comp_E(self, t:float) -> float:
+        return self._af(t) * self.E_act + (1.0 - self._af(t)) * self.E_pas
+    
+    def comp_dEdt(self, t:float) -> float:
+        return (self.comp_E(t + self.eps) - self.comp_E(t - self.eps)) / 2.0 / self.eps
+    
+    def comp_dvdt(self, q_in:float, q_out:float) -> float:
+        return q_in - q_out
+    
+    def comp_dpdt(self, t:float, q_in:float, q_out:float) -> float:
+        dvdt = self.comp_dvdt(q_in=q_in, q_out=q_out)
+        dEdt = self.comp_dEdt(t)
+        
+        
+        
+        
+class OdeModel():
+    def __init__(self, time_setup_dict) -> None:
+        self.time_object = TimeClass(time_setup_dict=time_setup_dict)
+    
+    def connect_modules(self, module1:Chamber, module2:Chamber) ->None:
+        module2._state_var['Q_i'] = module1.Q_o
+        module2._state_var['P_o'] = module1.P_o
+    
+class NaghaviModel(OdeModel):
+    def __init__(self, time_setup_dict) -> None:
+        super().__init__(time_setup_dict)
+        
+        # define a set of main variables
+        self._main_variable_keys = {
+            'p_lv', # pressure in the left ventricle
+            'p_la', # pressure in the left atrium
+            'p_ao', # pressure in the aorta
+            'p_ar', # pressure in the arteries
+            'p_lv', # pressure in the vena cava
+        }
+        
+        self.av = Valve_leaky_diode(time_object=self.time_object,
+                                    r = 1.0,
+                                    max_func=relu_max
+                                    )
+        
+        self.ao = Rc_component(time_object=self.time_object, 
+                                  r = 1.0,
+                                  c = 1.0, 
+                                  v_ref = 1.0,
+                                  )
+        
+        self.art = Rc_component(time_object=self.time_object,
+                                r = 1.0,
+                                c = 1.0,
+                                v_ref= 1.0,
+                                )
+        
+        self.ven = Rc_component(time_object=self.time_object,
+                                r = 1.0,
+                                c = 1.0,
+                                v_ref= 1.0,
+                                )
+        
+        self.connect_modules(self.ao,  self.art)
+        self.connect_modules(self.art, self.ven)
+        
+        
