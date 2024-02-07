633e1c0d6a7cf4d11b7d84c95449c08666bd6a48
Maximilian Balmus <balmus.maximilian@gmail.com>
2024-02-07 16:02:29 +0000

    circmodels: new class NaghaviModelParameters used to store the parameters of the NaghaviModel

--- ModularCirc/circmodels.py
+++ ModularCirc/circmodels.py
@@ -82,12 +82,90 @@ class OdeModel():
         for key in self._state_variable_dict.keys():
             out += f" - {key} \n"
         return out
-
+    
+    
+class NaghaviModelParameters():
+    def __init__(self) -> None:
+        components = ['ao', 'art', 'ven', 'av', 'mv', 'la', 'lv']
+        self.components = {key : None for key in components}
+        for key in ['ao', 'art', 'ven']:
+            self.components[key] = pd.Series(index=['r', 'c', 'v_ref'], dtype='float64')
+        for key in ['av', 'mv']:
+            self.components[key] = pd.Series(index=['r', 'max_func'], dtype=object)
+        for key in ['la', 'lv']:
+            self.components[key] = pd.Series(index=['E_pas', 'E_act', 'V_ref', 't_max', 't_tr', 'tau'], dtype='float64')
+                        
+        self.components['ao'].loc[:] =  [32000., 0.0025, 100.]
+        self.components['art'].loc[:] = [150000., 0.025, 900.]
+        self.components['ven'].loc[:] = [1200., 1., 2800.]
+        
+        self.components['av'].loc[:] = [800., relu_max]
+        self.components['mv'].loc[:] = [550., relu_max]
+        
+        self.components['la'].loc[:] = [60., 0.44/0.0075, 10., 150, 225., 25.]
+        self.components['lv'].loc[:] = [400, 1./0.0075, 10., 280., 420., 25.]
+        
+    def __repr__(self) -> str:
+        out = 'Naghavi Model parameter set: \n'
+        for comp in self.components: 
+            out += f" * Component - {comp}" + '\n'
+            for key, item in self.components[comp].items():
+                out += (f"  - {key:<10} : {item}") + '\n'
+            out += '\n'
+        return out
+    
+    def set_rc_comp(self, key:str, r:float=None, c:float=None, v_ref:float=None)->None:
+        if key not in ['ao','art', 'ven']:
+            raise Exception('Wrong key!')
+        if r is not None:
+            self.components[key].loc['r'] = r
+        if c is not None:
+            self.components[key].loc['c'] = c
+        if v_ref is not None:
+            self.components[key].loc['v_ref'] = v_ref
+        return
+            
+    def set_valve_comp(self, key:str, r:float=None, max_func=None)->None:
+        if key not in ['av', 'mv']:
+            raise Exception('Wrong key!')
+        if r is not None:
+            self.components[key].loc['r'] = r
+        if max_func is not None:
+            self.components[key].loc['max_func'] = max_func
+        return
+    
+    def set_chamber_comp(self, 
+                         key:str, 
+                         E_pas:float=None, 
+                         E_act:float=None, 
+                         V_ref:float=None, 
+                         t_max:float=None, 
+                         t_tr:float=None, 
+                         tau:float=None
+                         )->None:
+        if key not in ['lv', 'la']:
+            raise Exception('Wrong key!')
+        if E_pas is not None:
+            self.components[key].loc['E_pas'] = E_pas
+        if E_act is not None:
+            self.components[key].loc['E_act'] = E_act
+        if V_ref is not None:
+            self.components[key].loc['V_ref'] = V_ref
+        if t_max is not None:
+            self.components[key].loc['t_max'] = t_max
+        if t_tr is not None:
+            self.components[key].loc['t_tr'] = t_tr
+        if tau is not None:
+            self.components[key].loc['tau'] = tau
+        
     
 class NaghaviModel(OdeModel):
     def __init__(self, time_setup_dict,) -> None:
         super().__init__(time_setup_dict)
         self.name = 'NaghaviModel'
+        
+        base = NaghaviModelParameters()
+        print(base)
                 
         # Defining the aorta object
         self.commponents['ao'] = Rc_component( name='Aorta',
