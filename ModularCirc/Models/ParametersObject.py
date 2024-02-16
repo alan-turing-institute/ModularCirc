f5154728e0870fe6c022ed2dbb4b5e82b98e157d
Maximilian Balmus <balmus.maximilian@gmail.com>
2024-02-16 10:36:22 +0000

    ParametersObject: corrected assignment bug

--- ModularCirc/Models/ParametersObject.py
+++ ModularCirc/Models/ParametersObject.py
@@ -21,11 +21,14 @@ class ParametersObject():
     
     def __getitem__(self, key):
         return self.components[key]
+    
+    def __setitem__(self, key, val):
+        self.components[key] = val
         
     def _set_comp(self, key:str, set, **kwargs):
         if key not in set:
-            raise Exception('Wrong key!')
+            raise Exception(f'Wrong key! {key} not in {set}.')
         for k, val in kwargs.items():
             if val is None: continue
-            assert k in self[key].index
+            assert k in self[key].index.values
             self[key].loc[k] = val
