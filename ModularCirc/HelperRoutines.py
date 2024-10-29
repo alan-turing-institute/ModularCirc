a88ae7073b003575c252b144f2629d67036170ee
Maximilian Balmus <balmus.maximilian@gmail.com>
2024-10-29 10:42:42 +0000

    interim fix

--- ModularCirc/HelperRoutines.py
+++ ModularCirc/HelperRoutines.py
@@ -204,7 +204,9 @@ def simple_bernoulli_diode_flow(t:float,
     if y is not None:
         p_in, p_out = y[:2]
     dp   = p_in - p_out
-    return np.where(dp >= 0.0,  CQ * np.sqrt(dp), -CQ * RRA *np.sqrt(-dp))
+    return np.where(dp >= 0.0,  
+                    CQ * np.sqrt(np.where(dp >= 0, dp, 0)), 
+                   -CQ * RRA *np.sqrt(np.where(dp < 0, -dp, 0.0)))
 
 # @jit(cache=True, nopython=True)
 def maynard_valve_flow(t:float,
