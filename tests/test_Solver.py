6acafe01a31579309b992627964dac82067cce30
Levan Bokeria <levan.bokeria@gmail.com>
2025-03-07 15:35:15 +0000

    added a test for the solve() function

--- tests/test_Solver.py
+++ tests/test_Solver.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import json
 from ModularCirc.Models.OdeModel import OdeModel
 from ModularCirc.Solver import Solver
 from ModularCirc.Models.KorakianitisMixedModel import KorakianitisMixedModel
@@ -20,12 +21,18 @@ class TestSolver(unittest.TestCase):
             'dt': 0.001,
             'export_min': 1
         }
+
         # Initialize the parameter object
         self.parobj = KorakianitisMixedModel_parameters()
+
         # Initialize the model
         self.model = KorakianitisMixedModel(time_setup_dict=self.time_setup_dict, parobj=self.parobj, suppress_printing=True)
+ 
         # Initialize the solver
         self.solver = Solver(model=self.model)
+
+        # Setup the solver
+        self.solver.setup(suppress_output=True, method='LSODA')        
 
     def test_solver_initialization(self):
         # Verify solver is an instance of Solver
@@ -34,9 +41,6 @@ class TestSolver(unittest.TestCase):
         self.assertEqual(self.solver.model, self.model)
 
     def test_solver_setup(self):
-        
-        # Setup the solver
-        self.solver.setup(suppress_output=True, method='LSODA')
 
         # Verify the setup attributes
         self.assertEqual(self.solver._method, 'LSODA')
@@ -85,26 +89,51 @@ class TestSolver(unittest.TestCase):
         expected_output = np.load('tests/expected_outputs/expected_output_pv_dfdt_global.npy')
         np.testing.assert_allclose(pv_dfdt_result, expected_output)
 
+        # Test s_u_update():
 
-    def test_advance_cycle(self):
-        # Setup the solver
-        self.solver.setup(suppress_output=True, method='LSODA')
-        # Initialize the solution fields
-        self.solver._asd.loc[0, self.solver._initialize_by_function.index] = \
-            self.solver.initialize_by_function(y=self.solver._asd.loc[0].to_numpy()).T
-        # Advance one cycle
-        y0 = self.solver._asd.iloc[0, list(self.solver._global_psv_update_fun.keys())].to_list()
-        result = self.solver.advance_cycle(y0=y0, cycleID=0)
-        # Verify the result
-        self.assertFalse(result)
+        # Verify that s_u_update() accepts the expected input
+        # Load the expected values from an npy file
+        y_temp = np.load('tests/inputs_for_tests/inputs_for_s_u_update.npy')
+
+        # Verify the function can run with the expected input
+        s_u_result = self.solver.s_u_update(y_temp)
+
+        # Verify the output matches the expected output
+        expected_output = np.load('tests/expected_outputs/expected_output_s_u_update.npy')
+        np.testing.assert_allclose(s_u_result, expected_output)
+
 
     def test_solver_solve(self):
-        # Setup the solver
-        self.solver.setup(suppress_output=True, method='LSODA')
+
         # Solve the system
         self.solver.solve()
+
         # Verify the solver converged
         self.assertTrue(self.solver.converged or self.solver._Nconv is not None)
+
+        # Load expected values from a JSON file
+        with open('tests/expected_outputs/KorakianitisMixedModel_expected_output.json', 'r') as f:
+            self.expected_values = json.load(f)
+
+        # Redefine tind based on how many heart cycle have actually been necessary to reach steady state
+        self.tind_fin  = np.arange(start=self.model.time_object.n_t-self.model.time_object.n_c * self.model.time_object.export_min,
+                                   stop=self.model.time_object.n_t)
+        # Retrieve the component state variables, compute the mean of the values during the last cycle and store them within
+        # the new solution dictionary
+        new_dict = {}
+        for key, value in self.model.components.items():
+
+            new_dict[key] = {
+                'V': value.V.values[self.tind_fin].mean(),
+                'P_i': value.P_i.values[self.tind_fin].mean(),
+                'Q_i': value.Q_i.values[self.tind_fin].mean()
+            }
+
+        # Check that the values are the same as the expected values
+        expected_ndarray = np.array([self.expected_values[key1][key2]  for key1 in new_dict.keys() for key2 in new_dict[key1].keys()])
+        new_ndarray      = np.array([new_dict[key1][key2]              for key1 in new_dict.keys() for key2 in new_dict[key1].keys()])
+        test_ndarray     = np.where(np.abs(expected_ndarray) > 1e-6, np.abs((expected_ndarray - new_ndarray) / expected_ndarray),  np.abs((expected_ndarray - new_ndarray)))
+        self.assertTrue((test_ndarray < 1e-3).all())
 
 if __name__ == '__main__':
     unittest.main()
