5a63ddb57a69fdfcdb87d7a3ac566e21953af3f2
Maximilian <balmus.maximilian@gmail.com>
2025-10-28 17:31:57 +0000

    test

--- src/ModularCirc/Components/_ComponentFactoriesOptimized.py
+++ src/ModularCirc/Components/_ComponentFactoriesOptimized.py
@@ -98,7 +98,7 @@ class ComponentFunctionFactory:
         GenTimeShifter is used instead of partial(time_shift, ...) because it provides
         better performance and avoids closure overhead for repeated (delay, T) pairs.
         """
-        return GenTimeShifter(delay, T)
+        return GenTimeShifter(time_shift=delay, T=T)
         
     
     @staticmethod
